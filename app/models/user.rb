d195049c0fb8248a61058979b5d6ea5bf4d3326a
s246127 <hiroki71027923@icloud.com>
2021-02-14 11:58:36 +0900

    新規登録、ログイン機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, precense: true
+
+  has_many :teams, through: :team_users
+  has_many :team_users
+  has_many :posts
+  has_many :comments
+end
