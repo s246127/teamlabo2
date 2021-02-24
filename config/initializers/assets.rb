1b5fc301798aaf1273f6a4c75d254aaef1507645
s246127 <hiroki71027923@icloud.com>
2021-02-24 17:57:49 +0900

    assets route error解決できず

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.paths << Rails.root.join('app/assets/config')
+Rails.application.config.assets.paths << Rails.root.join('config')
+Rails.application.config.assets.paths << Rails.root.join('demo')
+Rails.application.config.assets.paths << Rails.root.join('images')
+Rails.application.config.assets.paths << Rails.root.join('img')
+Rails.application.config.assets.paths << Rails.root.join('js')
+Rails.application.config.assets.paths << Rails.root.join('scss')
+Rails.application.config.assets.paths << Rails.root.join('stylesheets')
+Rails.application.config.assets.precompile += %w( *.eot *.woff *.ttf *.svg *.otf *.png *.jpg *.jpeg *.gif vendor.css vendor.js )
